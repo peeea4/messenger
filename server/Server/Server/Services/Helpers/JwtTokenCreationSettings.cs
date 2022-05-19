fb9333e16ce1ea45336c846f82eb81efb118aff9
Stanislav Dechko <stas.dechko@gmail.com>
2022-05-19 16:27:08 +0300

    add jwtCreationSettings

---
+++ server/Server/Server/Services/Helpers/JwtTokenCreationSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Server.Services
+{
+    public static class JwtTokenCreationSettings
+    {
+        public static string Issuer => "ISSUER";
+
+        public static string Audience => "AUDIENCE";
+
+        public static DateTime? Expires => DateTime.Now.AddMinutes(60);
+        
+        public static SigningCredentials SigningCredentials => new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes("mysupersecret_secretkey!123")),
+            SecurityAlgorithms.HmacSha256); 
+    }
+}
